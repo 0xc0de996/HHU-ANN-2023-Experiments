bc9af95c4d5041668857aac87aeb1a63f74681d4
0xC0DE <1124887316@qq.com>
2023-03-09 15:25:38 +0800

    experiment02

--- experiment02/experiment02.py
+++ experiment02/experiment02.py
@@ -1,5 +1,15 @@
-import torch
+try:
+    import torch
+except ImportError as e:
+    os.system("pip install torch -i https://pypi.tuna.tsinghua.edu.cn/simple")
+    import torch
 
+try:
+    import torchvision
+except ImportError as e:
+    os.system("pip install torchvision -i https://pypi.tuna.tsinghua.edu.cn/simple")
+    import torchvision
+    
 def main():
     return torch.tensor(0)
     
